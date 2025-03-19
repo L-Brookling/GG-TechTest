7d86c9230436bd675651a55bc60928764e5436a5
Louis <178508612+L-Brookling@users.noreply.github.com>
2025-03-19 14:07:55 +0000

    Update weatherInfoDiv

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ document.addEventListener("DOMContentLoaded", function () {
   async function fetchWeather() {
     try {
       const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`
+        `https://europe-west1-amigo-actions.cloudfunctions.net/recruitment-mock-weather-
+endpoint/forecast?appid=a2ef86c41a&lat=27.987850&lon=86.925026`
       );
       const data = await response.json();
 
